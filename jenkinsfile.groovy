13a40ba023e2a7cdd98f468b211f1b631975198a
khadkeaakash <khadke_aakash@yahoo.co.in>
2020-07-18 12:06:00 +0800

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -12,8 +12,8 @@ pipeline {
                 #docker build --tag ubuntu-original:focal --file
                 curl -s 'https://partner-images.canonical.com/core/focal/current/ubuntu-focal-core-cloudimg-amd64-root.tar.gz' -o ubuntu-focal-core-cloudimg-amd64-root.tar.gz  > /dev/null
                 ls -al
-                #docker build --no-cache -t ubuntu-original:focal -f ubuntu-focal-original .
-                docker build --no-cache -t ubuntu-original:focal -f ubuntu-focal-secure .
+                docker build --no-cache -t ubuntu-original:focal -f ubuntu-focal-original .
+                docker build --no-cache -t ubuntu-secure:focal -f ubuntu-focal-secure .
                 
                 '''
             } //end of steps
